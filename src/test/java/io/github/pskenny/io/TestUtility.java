3985e9b8f00b96da4b4ef4e169017062f6eac32c
Paul Kenny <paul.s.kenny@gmail.com>
2019-11-28 14:49:39 +0000

    Add initial unimplemented test class for Utility

---
+++ src/test/java/io/github/pskenny/io/TestUtility.java
@@ -0,0 +1,14 @@
+package io.github.pskenny.io;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestUtility {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
